1b3da53872e74864ac1af92760c85d000919b83e
Antony dos santos felisberto <88542984+AntonyFelisberto@users.noreply.github.com>
2024-04-08 19:35:55 -0300

    incluindo components

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # projeto_todo_react
  
 npx create-react-app todoapp
+npm run start
---
+++ projeto/todoapp/src/components/FirstComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function FirstComponent() {
+  return (
+    <div className='firstComponent'>
+        {/* INICIANDO COMPONENT */}
+        <p>Primeiro component</p>
+    </div>
+  )
+}
+
+export default FirstComponent
